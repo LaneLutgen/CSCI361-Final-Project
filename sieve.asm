4b78af8e388bc203a6b295eadf59e6de126944c9
Lane Lutgen <lutgenlane@gmail.com>
2015-11-27 16:18:03 -0700

    Small improvement

--- sieve.asm
+++ sieve.asm
@@ -8,7 +8,6 @@ space:	.asciiz	" "		# whitespace to separate prime numbers
 main:	li	$s0, 0x11111111	# initialize $s0 with ones
 	li	$s1, 0x00000000	# initialize $s1 with zeroes
 	li	$t9, 200	# find prime numbers from 2 to $t9
-
 	add	$s2, $sp, 0	# backup bottom of stack address in $s2
 	li	$t0, 1		# reset counter variable to 1
 
@@ -23,14 +22,14 @@ check:	add	$t2, $s2, 0	# save the bottom of stack address to $t2
 
 	lw	$t3, ($t2)	# load the content into $t3
 
-	beq	$t3, $s0, outer	# only 0's? go back to the outer loop
+	beq	$t3, $s0, outer	# only 1's? go back to the outer loop
 
 inner:	add	$t2, $s2, 0	# save the bottom of stack address to $t2
 	mul	$t3, $t1, 4	# calculate the number of bytes to jump over
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
 	add	$t2, $t2, 8	# add 2 words - we started counting at 2!
 
-	sw	$s0, ($t2)	# store 0's -> it's not a prime number!
+	sw 	$s0, ($t2)	# store 1's -> it's not a prime number!
 
 	add	$t1, $t1, $t0	# do this for every multiple of $t0
 	bgt	$t1, $t9, outer	# every multiple done? go back to outer loop
@@ -48,12 +47,12 @@ count:	add	$t0, $t0, 1	# increment counter variable (start at 2)
 	add	$t2, $t2, 8	# add 2 words - we started counting at 2!
 
 	lw	$t3, ($t2)	# load the content into $t3
-	beq	$t3, $s0, count	# only 0's? go back to count loop
+	beq	$t3, $s0, count	# only 1's? go back to count loop
 
 	add	$t3, $s2, 0	# save the bottom of stack address to $t3
 
 	sub	$t3, $t3, $t2	# substract higher from lower address (= bytes)
-	div	$t3, $t3, 4	# divide by 4 (bytes) = distance in words
+	srl	$t3, $t3, 2	# divide by 4 (bytes) = distance in words
 	add	$t3, $t3, 2	# add 2 (words) = the final prime number!
 
 	li	$v0, 1		# system code to print integer
