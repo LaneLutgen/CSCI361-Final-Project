32a449b2c7b76dfd2a527beb6370b3262f3b5e9d
John <jcwaters158@gmail.com>
2015-12-01 13:46:50 -0700

    Little lower, but still missing the cache on the inner loop

--- sieve.asm
+++ sieve.asm
@@ -14,7 +14,9 @@ main:	li	$s0, 0x11111111	# initialize $s0 with ones
 outer:	addi  	$t0, $t0, 1	# increment counter variable (start at 2)
 	#mul	$t1, $t0, 2	# multiply $t0 by 2 and save to $t1
 	sll  	$t1, $t0, 1
-	bgt	$t1, $t9, print	# start printing prime numbers when $t1 > $t9
+	blt 	$t1, $t9, check
+	#bgt	$t1, $t9, print	# start printing prime numbers when $t1 > $t9
+	j	print
 
 check:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	#mul	$t3, $t0, 4	# calculate the number of bytes to jump over
@@ -32,13 +34,16 @@ inner:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
 	addi	$t2, $t2, 8	# add 2 words - we started counting at 2!
 
-	sw 	$s0, ($t2)	# store 1's -> it's not a prime number!
+
+	sw 	$s0, ($t2)	# store 1's -> it's not a prime number!	
 
 	add	$t1, $t1, $t0	# do this for every multiple of $t0
-	bgt	$t1, $t9, outer	# every multiple done? go back to outer loop
-
-	j	inner		# some multiples left? go back to inner loop
-
+	#bgt	$t1, $t9, outer	# every multiple done? go back to outer loop
+	blt	$t1, $t9, inner
+	
+	#b	inner		# some multiples left? go back to inner loop
+	j	outer
+	
 print:	li	$t0, 1		# reset counter variable to 1
 count:	addi	$t0, $t0, 1	# increment counter variable (start at 2)
 
@@ -70,7 +75,8 @@ skip:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	la	$a0, space	# the argument will be a whitespace
 	syscall			# print it!
 
-	ble	$t0, $t9, count	# take loop while $t0 <= $t9
+	#ble	$t0, $t9, count	# take loop while $t0 <= $t9
+	j 	count
 
 #exit:	li	$v0,10		# set up system call 10 (exit)
 #	syscall	
