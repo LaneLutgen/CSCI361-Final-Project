78d4d69c81eac8253782f772dc4c4275711e7798
John Waters <jcwaters158@gmail.com>
2015-12-02 09:18:18 -0700

    Restructured
    the print loop and changed some branches. Shaved off another ~2000

--- sieve.asm
+++ sieve.asm
@@ -12,47 +12,39 @@ main:	li	$s0, 0x11	# initialize $s0 with ones
 
 outer:	addi  	$t0, $t0, 1	# increment counter variable (start at 2)
 	sll  	$t1, $t0, 1	# multiply $t0 by 2 and save to $t1	
-	blt 	$t1, $t9, check	
+	bne 	$t1, $t9, check	
 	j	print		# start printing prime numbers when $t1 > $t9
 
 check:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	addi	$t3, $t0, 1
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
-	addi	$t2, $t2, 2	# add 2 words - we started counting at 2!
 
-	lb	$t3, ($t2)	# load the content into $t3
+	lb	$t3, 2($t2)	# load the content into $t3
 
 	beq	$t3, $s0, outer	# only 1's? go back to the outer loop
 
 inner:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	addi 	$t3, $t1, 1
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
-	addi	$t2, $t2, 2	# add 2 words - we started counting at 2!
 	
-	sb	$s0, 0($t2)	# store 1's -> it's not a prime number!	
+	sb	$s0, 2($t2)	# store 1's -> it's not a prime number!	
 
 	add	$t1, $t1, $t0	# do this for every multiple of $t0
 	blt	$t1, $t9, inner
 	
 	j	outer
 	
-print:	li	$t0, 1		# reset counter variable to 1
-count:	addi	$t0, $t0, 1	# increment counter variable (start at 2)
-
-	blt	$t0, $t9, skip	
-	
-				# make sure to exit when all numbers are done
-	li	$v0,10		# set up system call 10 (exit)
-	syscall	
+print:	li	$t0, 2		# reset counter variable to 1
 
 skip:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	addi	$t3, $t0, 1
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
 	addi	$t2, $t2, 2	# add 2 words - we started counting at 2!
 
-	lb	$t3, ($t2)	# load the content into $t3
-	beq	$t3, $s0, count	# only 1's? go back to count loop
+	lb	$t3, ($t2)	# load the content into $t3	
 	
+	addi	$t0, $t0, 1	# increment counter
+	beq	$t3, $s0, skip	# only 1's? go back to count loop	
 	addi	$t3, $s2, 0	# save the bottom of stack address to $t3
 
 	sub	$t3, $t3, $t2	# substract higher from lower address (= bytes)
@@ -67,4 +59,8 @@ skip:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	la	$a0, space	# the argument will be a whitespace
 	syscall			# print it!
 
-	j 	count
+
+	bne	$t0, $t9, skip	# repeat until $t0 = $t9
+
+exit:	li	$v0, 10		# set up system call 10 (exit)
+	syscall	
