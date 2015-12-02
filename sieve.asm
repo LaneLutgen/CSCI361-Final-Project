a03424378877305ad1bbb8327c99e30cbc91c26e
John Waters <jcwaters158@gmail.com>
2015-12-01 17:12:17 -0700

    little more...

--- sieve.asm
+++ sieve.asm
@@ -12,14 +12,11 @@ main:	li	$s0, 0x11111111	# initialize $s0 with ones
 	li	$t0, 1		# reset counter variable to 1
 
 outer:	addi  	$t0, $t0, 1	# increment counter variable (start at 2)
-	#mul	$t1, $t0, 2	# multiply $t0 by 2 and save to $t1
-	sll  	$t1, $t0, 1
-	blt 	$t1, $t9, check
-	#bgt	$t1, $t9, print	# start printing prime numbers when $t1 > $t9
-	j	print
+	sll  	$t1, $t0, 1	# multiply $t0 by 2 and save to $t1	
+	blt 	$t1, $t9, check	
+	j	print		# start printing prime numbers when $t1 > $t9
 
 check:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
-	#mul	$t3, $t0, 4	# calculate the number of bytes to jump over
 	sll	$t3, $t0, 2
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
 	addi	$t2, $t2, 8	# add 2 words - we started counting at 2!
@@ -29,38 +26,34 @@ check:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	beq	$t3, $s0, outer	# only 1's? go back to the outer loop
 
 inner:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
-	#mul	$t3, $t1, 4	# calculate the number of bytes to jump over
 	sll 	$t3, $t1, 2
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
-	addi	$t2, $t2, 8	# add 2 words - we started counting at 2!
-
-
-	sw 	$s0, ($t2)	# store 1's -> it's not a prime number!	
+	#addi	$t2, $t2, 8	# add 2 words - we started counting at 2!
+	
+	sw 	$s0, 8($t2)	# store 1's -> it's not a prime number!	
 
 	add	$t1, $t1, $t0	# do this for every multiple of $t0
-	#bgt	$t1, $t9, outer	# every multiple done? go back to outer loop
 	blt	$t1, $t9, inner
 	
-	#b	inner		# some multiples left? go back to inner loop
 	j	outer
 	
 print:	li	$t0, 1		# reset counter variable to 1
 count:	addi	$t0, $t0, 1	# increment counter variable (start at 2)
 
-	blt	$t0, $t9, skip	# make sure to exit when all numbers are done
+	blt	$t0, $t9, skip	
 	
+				# make sure to exit when all numbers are done
 	li	$v0,10		# set up system call 10 (exit)
 	syscall	
 
 skip:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
-	#mul	$t3, $t0, 4	# calculate the number of bytes to jump over
 	sll	$t3, $t0, 2
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
 	addi	$t2, $t2, 8	# add 2 words - we started counting at 2!
 
 	lw	$t3, ($t2)	# load the content into $t3
 	beq	$t3, $s0, count	# only 1's? go back to count loop
-
+	
 	addi	$t3, $s2, 0	# save the bottom of stack address to $t3
 
 	sub	$t3, $t3, $t2	# substract higher from lower address (= bytes)
@@ -75,8 +68,4 @@ skip:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
 	la	$a0, space	# the argument will be a whitespace
 	syscall			# print it!
 
-	#ble	$t0, $t9, count	# take loop while $t0 <= $t9
 	j 	count
-
-#exit:	li	$v0,10		# set up system call 10 (exit)
-#	syscall	
