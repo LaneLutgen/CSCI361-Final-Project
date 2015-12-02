72260ddcb36cdc757870bf1604493f17dd776f72
Lane Lutgen <lutgenlane@gmail.com>
2015-12-02 08:22:11 -0700

    Changed it so it stores bytes instead of words for better cache performance
    Down to 14000 cycles, 85% cache hit

--- sieve.asm
+++ sieve.asm
@@ -5,8 +5,7 @@ space:	.asciiz	" "		# whitespace to separate prime numbers
 
 	.text			# the text segment to store instructions
 	.globl 	main		# define main to be a global label
-main:	li	$s0, 0x11111111	# initialize $s0 with ones
-	li	$s1, 0x00000000	# initialize $s1 with zeroes
+main:	li	$s0, 0x11	# initialize $s0 with ones
 	li	$t9, 200	# find prime numbers from 2 to $t9
 	addi	$s2, $sp, 0	# backup bottom of stack address in $s2
 	li	$t0, 1		# reset counter variable to 1
@@ -17,20 +16,20 @@ outer:	addi  	$t0, $t0, 1	# increment counter variable (start at 2)
 	j	print		# start printing prime numbers when $t1 > $t9
 
 check:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
-	sll	$t3, $t0, 2
+	addi	$t3, $t0, 1
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
-	addi	$t2, $t2, 8	# add 2 words - we started counting at 2!
+	#addi	$t2, $t2, 2	# add 2 words - we started counting at 2!
 
-	lw	$t3, ($t2)	# load the content into $t3
+	lb	$t3, ($t2)	# load the content into $t3
 
 	beq	$t3, $s0, outer	# only 1's? go back to the outer loop
 
 inner:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
-	sll 	$t3, $t1, 2
+	addi 	$t3, $t1, 1
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
-	#addi	$t2, $t2, 8	# add 2 words - we started counting at 2!
+	addi	$t2, $t2, 2	# add 2 words - we started counting at 2!
 	
-	sw 	$s0, 8($t2)	# store 1's -> it's not a prime number!	
+	sb	$s0, 0($t2)	# store 1's -> it's not a prime number!	
 
 	add	$t1, $t1, $t0	# do this for every multiple of $t0
 	blt	$t1, $t9, inner
@@ -47,17 +46,17 @@ count:	addi	$t0, $t0, 1	# increment counter variable (start at 2)
 	syscall	
 
 skip:	addi	$t2, $s2, 0	# save the bottom of stack address to $t2
-	sll	$t3, $t0, 2
+	addi	$t3, $t0, 1
 	sub	$t2, $t2, $t3	# subtract them from bottom of stack address
-	addi	$t2, $t2, 8	# add 2 words - we started counting at 2!
+	addi	$t2, $t2, 2	# add 2 words - we started counting at 2!
 
-	lw	$t3, ($t2)	# load the content into $t3
+	lb	$t3, ($t2)	# load the content into $t3
 	beq	$t3, $s0, count	# only 1's? go back to count loop
 	
 	addi	$t3, $s2, 0	# save the bottom of stack address to $t3
 
 	sub	$t3, $t3, $t2	# substract higher from lower address (= bytes)
-	srl	$t3, $t3, 2	# divide by 4 (bytes) = distance in words
+	subi	$t3, $t3, 1	# divide by 4 (bytes) = distance in words
 	addi	$t3, $t3, 2	# add 2 (words) = the final prime number!
 
 	li	$v0, 1		# system code to print integer
