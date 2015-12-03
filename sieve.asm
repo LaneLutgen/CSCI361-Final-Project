8a2774527403a92b146c5bbd90c4e55c0fb0d05b
John Waters <jcwaters158@gmail.com>
2015-12-02 17:04:07 -0700

    little more

--- sieve.asm
+++ sieve.asm
@@ -10,30 +10,34 @@ main:	li	$s0, 0x11	# initialize $s0 with ones
 	addi	$s2, $sp, 0	# backup bottom of stack address in $s2
 	li	$t0, 1		# reset counter variable to 1
 
-outer:	addi  	$t0, $t0, 1	# increment counter variable (start at 2)
-	sll  	$t1, $t0, 1	# multiply $t0 by 2 and save to $t1	
-	bne 	$t1, $t9, check	
-	j	print		# start printing prime numbers when $t1 > $t9
+#outer:	addi  	$t0, $t0, 1	# increment counter variable (start at 2)
+#	sll  	$t1, $t0, 1	# multiply $t0 by 2 and save to $t1	
+#	bne 	$t1, $t9, check	
+#	j	print		# start printing prime numbers when $t1 > $t9
 
-check:	addi	$t3, $t0, 1
+check:	addi  	$t0, $t0, 1	# increment counter variable (start at 2)
+	sll  	$t1, $t0, 1	# multiply $t0 by 2 and save to $t1
+	beq	$t1, $t9, print	
+	
+	addi	$t3, $t0, 1
 	sub	$t2, $s2, $t3	# subtract them from bottom of stack address
 
 	lb	$t3, 2($t2)	# load the content into $t3
 
-	beq	$t3, $s0, outer	# only 1's? go back to the outer loop
-
+	beq	$t3, $s0, check	# only 1's? go back to the outer loop
+	
 inner:	addi 	$t3, $t1, 1
 	sub	$t2, $s2, $t3	# subtract them from bottom of stack address
 	
 	sb	$s0, 2($t2)	# store 1's -> it's not a prime number!	
 
 	add	$t1, $t1, $t0	# do this for every multiple of $t0
+	
 	blt	$t1, $t9, inner
 	
-	j	outer
+	j	check
 	
 print:	li	$t0, 2		# reset counter variable to 1
-
 skip:	addi	$t3, $t0, 1
 	sub	$t2, $s2, $t3	# subtract them from bottom of stack address
 	addi	$t2, $t2, 2	# add 2 words - we started counting at 2!
@@ -45,8 +49,7 @@ skip:	addi	$t3, $t0, 1
 	addi	$t3, $s2, 0	# save the bottom of stack address to $t3
 
 	sub	$t3, $t3, $t2	# substract higher from lower address (= bytes)
-	subi	$t3, $t3, 1	# divide by 4 (bytes) = distance in words
-	addi	$t3, $t3, 2	# add 2 (words) = the final prime number!
+	addi	$t3, $t3, 1	# add 2 (words) = the final prime number!
 
 	li	$v0, 1		# system code to print integer
 	addi	$a0, $t3, 0	# the argument will be our prime number in $t3
@@ -55,7 +58,6 @@ skip:	addi	$t3, $t0, 1
 	li	$v0, 4		# system code to print string
 	la	$a0, space	# the argument will be a whitespace
 	syscall			# print it!
-
 
 	bne	$t0, $t9, skip	# repeat until $t0 = $t9
 
